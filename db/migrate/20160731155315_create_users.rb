07719bda201905f1f4578d53b0fdd5d1ebcbefa1
Gregory Berns-Leone <gberns.l@gmail.com>
2016-07-31 18:24:10 -0500

    Add migrations

--- db/migrate/20160731155315_create_users.rb
+++ db/migrate/20160731155315_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration 
   def change 
     create_table :users do |t|
-      t.string :name { null: false, limit: 64 }
+      t.string :name, { null: false, limit: 64 }
 
       t.timestamps
+    end
   end 
 end
--- db/migrate/20160731181245_create_reviews.rb
+++ db/migrate/20160731181245_create_reviews.rb
@@ -1,4 +1,15 @@
 class CreateReviews < ActiveRecord::Migration 
   def change 
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :coffee_id
+      t.integer :preparer_id
+      t.integer :preparation_id
+      t.datetime :date
+      t.integer :rating, { null: false }
+      t.string :comments
+
+      t.timestamps
+    end
   end 
 end
